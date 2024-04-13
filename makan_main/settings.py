c78a4bed77ef3356080d71c88c8d1147031dfd6a
rolandoimmanuel <rolando.immanuel@gmail.com>
2024-04-13 19:34:32 +0700

    13 Apr 2024

--- makan_main/settings.py
+++ makan_main/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'makan_main'
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -123,6 +126,9 @@ STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'makan_main/static'
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
