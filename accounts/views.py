6036c69690c91b51fe29262731de61aaa3761597
rolandoimmanuel <rolando.immanuel@gmail.com>
2024-04-15 14:59:39 +0700

    register page and homepage css

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
+
 
 # Create your views here.
+
+def registerUser(request): 
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'your account successfully registered')
+            return redirect('registerUser')
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/registerUser.html', context)
